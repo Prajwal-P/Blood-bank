0113862873d3886c668ee697ae9843dd7b8375f8
Revanthpn <56716382+Revanthpn@users.noreply.github.com>
2019-11-24 01:48:59 +0530

    signup pagr first view

--- BloodBank/Bloodbank/user.xaml.cs
+++ BloodBank/Bloodbank/user.xaml.cs
@@ -24,5 +24,10 @@ namespace BloodBank
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
