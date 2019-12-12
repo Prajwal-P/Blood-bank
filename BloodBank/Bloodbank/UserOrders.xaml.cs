af3b8f02a778cc5a6e0fd54318097c7dab505007
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2019-12-12 22:11:25 +0530

    Hospital orders page compeleted

--- BloodBank/Bloodbank/UserOrders.xaml.cs
+++ BloodBank/Bloodbank/UserOrders.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BloodBank
 {
@@ -47,7 +36,7 @@ namespace BloodBank
                     NoData.Visibility = Visibility.Visible;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
