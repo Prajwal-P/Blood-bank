e877583cdd3a7f7b51ffb7a5bd584709956bc51e
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2019-12-13 02:40:10 +0530

    Only numbers can be enterd in Phone number text box

--- BloodBank/Bloodbank/med_inst.xaml.cs
+++ BloodBank/Bloodbank/med_inst.xaml.cs
@@ -216,5 +216,11 @@ namespace BloodBank
                 }
             }
         }
+
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
     }
 }
